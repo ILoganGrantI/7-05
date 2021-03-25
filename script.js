11bad13bf7b1fbdb53f25b765b5fee4049571027
Logan Grant <logan.grant1@ocsbstudent.ca>
2021-03-25 16:03:44 +0000

    7-05

--- script.js
+++ script.js
@@ -2,14 +2,23 @@ let dof = 0
 let age = 0
 
 document.getElementById('b2').addEventListener('click', click)
+document.getElementById('b1').addEventListener('click', click1)
 
 function click () {
-	if (document.getElementById('dayofweek').innerHTML = 'It is a weekday and age is under 18')
-	alert('Time for school!')
-} if else (document.getElementById('dayofweek').innerHTML = 'It is a weekday and age is not under 18') {
-	alert('Time to go to work!')
-} if else (document.getElementById('dayofweek').innerHTML = 'It`s not a weekday and age is under 18') {
-  alert('Time to relax for the weekend!')
-} if else (document.getElementById('dayofweek').innerHTML = 'It`s not a weekday and age is not under 18') {
-  alert('Time to relax for the weekend!')
+	dof = document.getElementById('dayofweek').value
+  if ( dof === 'Monday'|| dof ==='Tuesday'|| dof ==='Wednesday'|| dof ==='Thursday'|| dof ==='Friday' ) {
+  alert('Time for school!')
+} else if (dof === 'Saturday'||dof === 'Sunday') {
+  alert('weekend')
+}
+}
+
+function click1 () {
+	age = document.getElementById('age').value
+	age = parseInt(age)
+	if (age >= 18) {
+		alert('Time to go to work!')
+} else if (age <= 18) {
+		alert('Time for school!')
+	}
 }
