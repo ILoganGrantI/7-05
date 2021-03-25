0fa1d8b1c981b56b2c99df35145f47ea3d0e029f
Logan Grant <logan.grant1@ocsbstudent.ca>
2021-03-25 13:12:27 +0000

    Initial commit

---
+++ script.js
@@ -0,0 +1,15 @@
+let dof = 0
+let age = 0
+
+document.getElementById('b2').addEventListener('click', click)
+
+function click () {
+	if (document.getElementById('dayofweek').innerHTML = 'It is a weekday and age is under 18')
+	alert('Time for school!')
+} if else (document.getElementById('dayofweek').innerHTML = 'It is a weekday and age is not under 18') {
+	alert('Time to go to work!')
+} if else (document.getElementById('dayofweek').innerHTML = 'It`s not a weekday and age is under 18') {
+  alert('Time to relax for the weekend!')
+} if else (document.getElementById('dayofweek').innerHTML = 'It`s not a weekday and age is not under 18') {
+  alert('Time to relax for the weekend!')
+}
