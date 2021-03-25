712426c072f457e6f8370e9b145f50a1db4eac5c
Logan Grant <logan.grant1@ocsbstudent.ca>
2021-03-25 16:33:20 +0000

    7-05

--- script.js
+++ script.js
@@ -5,20 +5,20 @@ document.getElementById('b2').addEventListener('click', click)
 document.getElementById('b1').addEventListener('click', click1)
 
 function click () {
-	dof = document.getElementById('dayofweek').value
-  if ( dof === 'Monday'|| dof ==='Tuesday'|| dof ==='Wednesday'|| dof ==='Thursday'|| dof ==='Friday' ) {
+  dof = document.getElementById('dayofweek').value
+  if ( dof === 'Monday' || dof === 'Tuesday' || dof ===  'Wednesday' || dof === 'Thursday' || dof === 'Friday' ) {
   alert('Time for school!')
-} else if (dof === 'Saturday'||dof === 'Sunday') {
+} else if (dof === 'Saturday' ||dof === 'Sunday') {
   alert('weekend')
 }
 }
 
 function click1 () {
-	age = document.getElementById('age').value
-	age = parseInt(age)
-	if (age >= 18) {
-		alert('Time to go to work!')
+  age = document.getElementById('age').value
+  age = parseInt(age)
+  if (age >= 18) {
+    alert('Time to go to work!')
 } else if (age <= 18) {
-		alert('Time for school!')
-	}
+    alert('Time for school!')
+  }
 }
